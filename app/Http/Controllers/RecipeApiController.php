d075f3a56d551096294f735276fe1a91ff16f4fc
Muhammad Arya Java <muhammad.aryajava@gmail.com>
2021-08-21 21:40:04 +0700

    perbaikan kecil

--- app/Http/Controllers/RecipeApiController.php
+++ app/Http/Controllers/RecipeApiController.php
@@ -20,18 +20,18 @@ class RecipeApiController extends Controller
     public function store(Request $request)
     {
         $recipe = Recipe::create($request->all());
-        return response()->json(['message' => 'Success', 'data' => $recipe]);
+        return response()->json(['message' => 'Add Success', 'data' => $recipe]);
     }
     public function update(Request $request, $id)
     {
         $recipe = Recipe::find($id);
         $recipe->update($request->all());
-        return response()->json(['message' => 'Success', 'data' => $recipe]);
+        return response()->json(['message' => 'Update Success', 'data' => $recipe]);
     }
     public function destroy($id)
     {
         $recipe = Recipe::find($id);
         $recipe->delete();
-        return response()->json(['message' => 'Success', 'data' => null]);
+        return response()->json(['message' => 'Delete Success', 'data' => null]);
     }
 }
